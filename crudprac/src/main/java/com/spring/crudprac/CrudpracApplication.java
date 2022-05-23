815ee4a1e9e40fc5458379cdf0406768f12cc543
ParkSungGyu1 <steven0507@naver.com>
2022-05-23 21:05:26 +0900

    프로젝트 생성 & 기초셋팅

---
+++ crudprac/src/main/java/com/spring/crudprac/CrudpracApplication.java
@@ -0,0 +1,14 @@
+package com.spring.crudprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudpracApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(CrudpracApplication.class, args);
+	}
+
+}

53b7b537ba1b1e02078765cbb61bc662195199c9
ParkSungGyu1 <steven0507@naver.com>
2022-05-24 09:48:30 +0900

    detail 전환 구현

--- crudprac/src/main/java/com/spring/crudprac/controller/PostController.java
+++ crudprac/src/main/java/com/spring/crudprac/controller/PostController.java
@@ -3,12 +3,14 @@ package com.spring.crudprac.controller;
 import com.spring.crudprac.dto.PostDto;
 import com.spring.crudprac.model.Posting;
 import com.spring.crudprac.service.PostingService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -31,15 +33,17 @@ public class PostController {
         return "main";
     }
 
-    @GetMapping("/detail")
-    public String detailview(){
-        return "detail";
-    }
 
     @PostMapping("/post/write")
     public String postwrite(@RequestBody PostDto postDto){
         postingService.postwrite(postDto);
         return "redirect:main";
+    }
+
+    @GetMapping("/post/detail")
+    public String detailview(@RequestParam Long id, Model model){
+        model.addAttribute("list",postingService.getdetail(id));
+        return "detail";
     }
 
 
--- crudprac/src/main/java/com/spring/crudprac/service/PostingService.java
+++ crudprac/src/main/java/com/spring/crudprac/service/PostingService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostingService {
@@ -25,5 +26,10 @@ public class PostingService {
 
     public List<Posting> postview() {
         return postRepository.findAll();
+    }
+
+    public Posting getdetail(Long id) {
+        Optional<Posting> result = postRepository.findById(id);
+        return result.get();
     }
 }
